e67cafe0ff9c6813d98aacff0cded9f919536643
OsuyaEmma <osuyae@emaris.com.ng>
2021-09-10 07:16:38 +0100

    added a new signup page

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Signup />
+
+  </div>
+</template>
+
+<script>
+import Signup from "./components/Signup";
+
+export default {
+  name: "App",
+  components: {
+    Signup,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <div>
+      <h1>Medix Ultimate</h1>
+      <h2>Version 3.5</h2>       
+      <p>Hospital Information management and accounting software</p>
+{{username}}
+{{password}}
+      <form @submit.prevent="signup">
+        <div>
+          <p style="font-family: Elephant; color: #008000;">User Name
+          <input type="text" v-model="username" placeholder="Username" required />
+          </p>
+        </div>
+        <div>
+          <p>password</p>
+          <p style="font-family: Elephant; color: #008000;">Password
+          <input type="password" v-model="password" placeholder="Password" required />
+        </p>
+        </div>
+        <div>
+          <button type="submit">
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    signup() {
+      console.log(this.username, this.password);
+    },
+  },
+};
+</script>
+
+<style></style>
